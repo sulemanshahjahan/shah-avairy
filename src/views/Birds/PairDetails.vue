b9873fc2ce696e4a673d0604a8ea8ea4077ed27b
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-08-29 01:18:37 +0500

    Added Birds inside PairDetails

--- src/views/Birds/PairDetails.vue
+++ src/views/Birds/PairDetails.vue
@@ -4,7 +4,13 @@
     <p><strong>Status: </strong> {{pair.status}}</p>
     <p><strong>Cage Number: </strong> {{pair.cageNumber}}</p>
     
-  <div class="">
+
+    <div class="birds">
+      <h2>Make & Female</h2>
+      <BirdCard v-for="bird in birds" :key="bird.id" :bird="bird" class="birdCards"/>
+    </div>
+
+  <div class="clutches">
     <h2>Clutches: </h2>
     <Clutches v-for="(clutch, index) in clutches" :key="index" :clutchids="index" :clutch="clutch" />
   </div>
@@ -14,18 +20,23 @@
 </template>
 <script>
 import ApiCall from '@/services/EventCalls'
+import BirdCard from '@/components/BirdCard.vue'
 import Clutches from '@/components/PairClutches.vue'
+import { watchEffect } from 'vue'
 
     export default{
       components:{
-        Clutches
+        Clutches,
+        BirdCard
 },
         props: ['cageNumber'],
         data(){
             return {
                 pair: null,
                 clutches: [],
-                clutchId: null
+                clutchId: null,
+                birdsID: null,
+                birds: []
             }
         },
         created() {
@@ -33,19 +44,56 @@ import Clutches from '@/components/PairClutches.vue'
     .then(response => {
       this.pair = response.data[0]
       this.clutchId = response.data[0].clutchId
+      this.birdsID = response.data[0].birdsID
+     
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  },
+  mounted   (){
+    watchEffect(() => {
+      
+
+     return ApiCall.getClutch(this.clutchId)
+    .then(response => {
+      this.clutches = response.data
+
+      ApiCall.getBirds(this.birdsID)
+    .then(response => {
+      this.birds = response.data
+     
     })
     .catch(error => {
       console.log(error);
     })
 
-    ApiCall.getClutch()
-    .then(response => {
-      this.clutches = response.data
     })
     .catch(error => {
       console.log(error);
     })
-    
+
+
+  })
+
+
   }
+ 
     }
 </script>
+
+<style>
+  .birds { 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 3%;
+}
+
+@media (max-width: 480px) {
+  .birds {
+    display: block;
+    column-gap: 0%;
+  }
+}
+</style>
