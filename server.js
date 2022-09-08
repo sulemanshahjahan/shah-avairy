3d70d67031c2b392706b42d5e8d8ad813758ec47
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-09 03:19:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
 
+http
+  .createServer((req, res) => {
+    fs.readFile('index.html', 'utf-8', (err, content) => {
+      if (err) {
+        console.log('We cannot open "index.html" file.')
+      }
 
-server.use(middlewares);
-server.use(router);
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      })
 
-server.listen(port);
+      res.end(content)
+    })
+  })
+  .listen(httpPort, () => {
+    console.log('Server listening on: http://localhost:%s', httpPort)
+  })
