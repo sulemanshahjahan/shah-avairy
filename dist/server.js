66a43ab11698b4c1007367ac4444ba03d39cd085
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-10 21:56:20 +0500

    Server updats

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,18 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 3000;
+
+console.log('server started '+ port);
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults();
-var port = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+app.listen(port);
