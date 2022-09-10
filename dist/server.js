3ca966c2940138f6a809cc9b875452100798dd1c
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-10 21:58:07 +0500

    s

--- dist/server.js
+++ dist/server.js
@@ -3,16 +3,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 3000;
-
+var port = process.env.PORT || 300;
+app.listen(port);
 console.log('server started '+ port);
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-app.listen(port);
