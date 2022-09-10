6cca3008acf69c9e7bf4fa1135672cc17c2ea2bc
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-10 21:00:48 +0500

    Adding dist folder

--- dist/server.js
+++ dist/server.js
@@ -1,27 +1,15 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('server started '+ port);
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-const http = require('http');
-const fs = require('fs');
 
-http
-  .createServer((req, res) => {
-    fs.readFile('index.html', 'utf-8', (err, content) => {
-      if (err) {
-        console.log('We cannot open "index.html" file.')
-      }
 
-      res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8',
-      })
-
-      res.end(content)
-    })
-  })
-
-server.use(middlewares);
 server.use(router);
-
-server.listen(port);
